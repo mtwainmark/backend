d93faf80053c1ffbad325903eb2489c23455b6ac
Nikita Skorokhod <mtwain.mark@gmail.com>
2021-11-03 21:50:43 +0300

    Jwt auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [UserModule, 
@@ -16,6 +17,6 @@ import { JwtModule } from '@nestjs/jwt';
       }
     })],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy]
+  providers: [AuthService, LocalStrategy, JwtStrategy]
 })
 export class AuthModule {}
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'test',
+    });
+  }
+
+  async validate(payload: {sub: number; username: string}) {
+      const user = await this.userService.findOne(payload.sub);
+      if(!user){
+          throw new UnauthorizedException("Unauthorized user")
+      }
+    return { id: payload.sub, email: payload.username };
+  }
+}
